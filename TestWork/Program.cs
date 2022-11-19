ff17a09f46a146d20be611e8b3b74105fc2b1d52
Maria <vilochka-17@yandex.ru>
2022-11-19 20:28:37 +0300

    Print and NewArray method made

--- TestWork/Program.cs
+++ TestWork/Program.cs
@@ -11,3 +11,23 @@ string[] CreateArray()
     string[] array = Console.ReadLine().Split(" ");
     return array;
 }
+
+string[] PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+    return array;
+}
+
+void NewArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write(array[i] + "\t");
+        }
+    }
+}
