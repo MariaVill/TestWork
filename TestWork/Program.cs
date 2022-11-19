63e71d91e55bccc0210853786db25a6917b3a831
Maria <vilochka-17@yandex.ru>
2022-11-19 20:27:11 +0300

    Create array made

--- TestWork/Program.cs
+++ TestWork/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+
+string[] CreateArray()
+{
+    Console.WriteLine("Введите эллементы массива через пробел:");
+    string[] array = Console.ReadLine().Split(" ");
+    return array;
+}
